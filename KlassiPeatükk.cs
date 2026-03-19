6dd746c8dd72eeacedfec693c66814d90b78672c
Opilane TTHK <opilane@tthk.ee>
2026-03-19 13:28:23 +0200

    lalala

---
+++ KlassiPeatükk.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsimeneProjekt // Nimeruum millises projektis (skoobis) antud klass on
+{
+    // Klassi anatoomia:
+    // A - Klassi enda nimeruum, mis sisaldab kõik mis klassil tema töö jaoks vaja on
+    //      Klassi nimeruum vajab
+    //      (A.1) - juurdepääsu modifikaatorit,
+    //      (A.2) - sätestust et tegu on klassiga
+    //      (A.3) - klassi enda nime.
+    // B - Klassi konstruktor defineerib ära spetsiaalse meetodi mis ütleb kuidas
+    //      selle klassi tüüpi objekt instantsieeritakse. Samamoodi nagu structil,
+    //      saab olla konstruktoreid mitu, erinevalt Structist, väärtused peab andma kaasa.
+    // C - Väljad mida konstruktor ootab, neile tuleb väärtus anda välja arvatud ühel juhul
+    // D - Klassi omadused. Nende kaudu saab kasutatavas koodis selle klassi tüüpi objekti
+    //      andmeid kätte, adresseerides neid punktiga. Erinevalt Structist, vajavad omadused
+    //      objekti tekitamisel mingit väärtust. Välja arvatud siis, kui omaduse
+    //      andmetüübi taga on küsimärk (?) mis ütleb, et see omadus võib jääda "null"iks.
+    //      (D.1)
+    //      Klassi omadusele saab anda vaikeväärtuse. Vaikeväärtus on midagi mis antakse
+    //      selle klassi tüüpi uuele objektile kaasa, selle objekti tekitamise hetkel, kui
+    //      ei ole sellele väljale väärtust antud.(D.2)
+    //A.1   A.2   A.3
+    public class KlassiPeatükk //A 
+    {
+        public KlassiPeatükk(string värvus, string materjal, double paksus) //B
+        {
+            Värvus = värvus; //C
+            Materjal = materjal;
+            Paksus = paksus;
+        }
+        //D
+        public string?/*D.1*/ Värvus { get; set; } 
+        public string Materjal { get; set; }
+        public double Paksus { get; set; } = -1;//D.2
+        //E klassi meetodid
+        /// <summary>
+        /// Returns a sentence, containing info about this object
+        /// </summary>
+        /// <returns>sentence</returns>
+        public string GetInfo()
+        {
+            return $"This object is made of {Materjal}, is {Paksus} thick and has color {Värvus}.";
+        }
+        /// <summary>
+        /// Method that return true, if this material can be dremeled or not<br/>
+        /// If the dremel is longer than the material is thick, then it is not dremelable, and vice-versa.
+        /// </summary>
+        /// <param name="freesipaksus">Input your dremel length</param>
+        /// <returns>true or false</returns>
+        public bool CanThisBeDremeled(double freesipaksus) 
+        {
+            if (freesipaksus < Paksus)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsimeneProjekt
+{
+    public enum CarMark
+    {
+        Toyota,Ford,Trashla,Subaru,Saab,Volvo,BMW,Mini,Škoda,
+    }
+    public class Vehicle 
+    {
+        public Vehicle(
+            string model, 
+            CarMark mark, 
+            string plate, 
+            string colour, 
+            decimal unloadedWeight, //one unit is one ton
+            List<string> equiptment)
+        {
+            Model = model;
+            Mark = mark;
+            LicensePlate = plate;
+            Colour = colour;
+            EmptyWeight = unloadedWeight;
+            Equiptment = equiptment;
+        }
+
+        public string Model { get; set; }
+        public CarMark Mark { get; set; }
+        public string LicensePlate { get; set; }
+        public string Colour { get; set; }
+        public decimal EmptyWeight { get; set; } = 0;
+        public List<string>? Equiptment { get; set; } 
+        /// <summary>
+        /// Returns info about this car
+        /// </summary>
+        /// <returns>Human-readable sentence as a string</returns>
+        public string GetInfo()
+        {
+            return $"This is a {Mark} {Model}, it is {Colour} color and weighs {EmptyWeight} Tons.\nLicense plate number is {LicensePlate}";
+        }
+        public void SeeEquiptment()
+        {
+            Console.WriteLine("This car has: ");
+            foreach (var thing in Equiptment)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
